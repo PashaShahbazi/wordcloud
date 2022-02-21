e9b9ffdcae8d1f874ed2916e43b62cf3fd483a8a
PashaShahbazi <pasha.shahbazii@gmail.com>
2022-02-21 16:39:43 +0330

    creating script for geting text from wikipedia

--- get_wiki.py
+++ get_wiki.py
@@ -1,3 +1,13 @@
 """ this file is just for search and get wikipedia
 information about the name from user """
+import wikipedia
 
+
+def wiki_get(subject):
+    """
+    (str) --> str
+    return the string text from wikipedia
+    >>>wiki_get('name_subject')
+    str text
+    """
+    return wikipedia.page(subject).content
