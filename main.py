c375a64379f06b0ab316d0211d3306f1e6feb4b3
PashaShahbazi <pasha.shahbazii@gmail.com>
2022-02-21 16:26:04 +0330

    importing the packages and  ask for file or subject to make wordcloud

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 """This main script is for generating a word cloud from the
 word you search for on Wikipedia or a text in a "txt" format that you have prepared yourself.
 """
+# Import needed packages
+from tkinter.filedialog import askopenfile, asksaveasfile
+import re
+import numpy as np
+import wordcloud
+import get_wiki as w
+import creat_wordcloud as cw
+import matplotlib.pyplot as mat
+import make_plot
+from PIL import Image
+
+wiki_or_file = input('Do you have a text file or do you want to search about a subject?\
+enter f for file or w for Wikipedia--> ')
+if wiki_or_file.lower() == 'f':
+    text = askopenfile(mode='r', title='Where is your file txt')
+    text = re.findall("[a-z A-Z]:.*t", str(text))
+    text1 = open(text[0]).readlines()
+    text = ''
+    for i in text1:
+        text += i
+
