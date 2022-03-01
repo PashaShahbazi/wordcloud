1df9a331db7e9ff7d7a910f625e6bf477ce98fa3
mohammadpasha shahbazi <pasha.shahbazii@gmail.com>
2022-03-01 18:07:26 +0330

    fix the bug in the first if in main.py

--- main.py
+++ main.py
@@ -39,8 +39,9 @@ background_color = input('What color do you want for back grand -->')
 if mask_or_not.lower() == 'n':
     print('Please wait.....')
     # get the path of return image with tkinter
-    save_path = asksaveasfile(title='Where you want to save?')
-    save_path = save_path + '.jpg'
+    print('Add the format you want to the end of the name, such as JPG or PNG')
+    save_path = asksaveasfile(title='Where you want to save?\
+     Add the format you want to the end of the name, such as JPG or PNG')
     # create the wordcloud from script create_wordcloud.py
     cloud = cw.create_cloud(text, bgcolor=background_color)
     # create plot from word cloud
@@ -54,6 +55,7 @@ elif mask_or_not.lower() == 'y':
     # ask for image path for open it for wordcloud
     image = Image.open(askopenfile(title='Where the image you want to use').name)
     mask = np.array(image)
+    print('Add the format you want to the end of the name, such as JPG or PNG')
     save_path = asksaveasfile(title='Where you want to save?')
     print('Please wait.....')
     cloud = cw.create_cloud_mask(text=text, bgcolor=background_color, mask=mask)
